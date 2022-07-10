ac8f1f2376f7de2007cc0d396edb359d1eb0a6e4
PamaWare <108025703+DynamicWare@users.noreply.github.com>
2022-07-10 10:23:21 -0700

    Update API.lua

--- API.lua
+++ API.lua
@@ -1,19 +1,1 @@
 
-function InfJumpOn()
-
-    _G.infinjump = true
-    local Player = game:GetService("Players").LocalPlayer
-    local Mouse = Player:GetMouse()
-    Mouse.KeyDown:connect(function(k)
-        if _G.infinjump then
-            if k:byte() == 32 then
-                Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-                Humanoid:ChangeState("Jumping")
-                wait()
-                Humanoid:ChangeState("Seated")
-            end
-        end
-
-    end)
-
-end
